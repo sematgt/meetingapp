1e5b6761e1e930ec9e6c9dbeaf42faf98cefce83
sgbliznyuk <sgbliznyuk@gmail.com>
2020-06-23 16:14:25 +0300

    the server-side is done

--- index.js
+++ index.js
@@ -36,3 +36,47 @@ class SkillShareServer {
         this.server.close();
     }
 }
+
+SkillShareServer.prototype.talkResponse = function() {
+    let talks = [];
+    for (let title of Object.keys(this.talks)) {
+        talks.push(this.talks[title]);
+    }
+    return {
+        body: JSON.stringify(talks),
+        headers: {"Content-Type": "application/json",
+        "ETag": `"${this.version}"`}
+    };
+};
+
+router.add("GET", /^\/talks$/, async (server, request) => {
+    let tag = /"(.*)"/.exec(request.headers["if-none-match"]);
+    let wait = /\bwait=(\d+)/.exec(request.headers["prefer"]);
+    if (!tag || tag[1] != server.version) {
+        return server.talkResponse();
+    } else if (!wait) {
+        return {status: 304};
+    } else {
+        return server.waitForChanges(Number(wait[1]));
+    }
+});
+    
+SkillShareServer.prototype.waitForChanges = function(time) {
+    return new Promise(resolve => {
+        this.waiting.push(resolve);
+        setTimeout(() => {
+            if (!this.waiting.includes(resolve)) return;
+            this.waiting = this.waiting.filter(r => r != resolve);
+            resolve({status: 304});
+        }, time * 1000);
+    });
+    };
+    
+SkillShareServer.prototype.updated = function() {
+    this.version++;
+    let response = this.talkResponse();
+    this.waiting.forEach(resolve => resolve(response));
+    this.waiting = [];
+};
+
+new SkillShareServer(Object.create(null)).start(8000);
