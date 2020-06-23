5d2404230d771cd0bbb066509c7f0d2d2d04956a
sgbliznyuk <sgbliznyuk@gmail.com>
2020-06-23 15:41:54 +0300

    added server class

---
+++ index.js
@@ -0,0 +1,38 @@
+const {createServer} = require("http");
+const Router = require("./router");
+const ecstatic = require("ecstatic");
+
+const router = new Router();
+const defaultHeaders = {"Content-Type": "text/plain"};
+
+class SkillShareServer {
+    constructor(talks) {
+        this.talks = talks;
+        this.version = 0;
+        this.waiting = [];
+
+        let fileServer = ecstatic({root: "./public"});
+        this.server = createServer((request, response) => {
+            let resolved = router.resolve(this, request);
+            if (resolved) {
+                resolved.catch(error => {
+                    if (error.status != null) return error;
+                    return {body: String(error), status: 500};
+                }).then(({body,
+                            status = 200,
+                            headers = defaultHeaders}) => {
+                            response.writeHead(status, headers);
+                            response.end(body);
+                });
+            } else {
+                fileServer(request, response);
+            }
+        });
+    }
+    start(port) {
+    this.server.listen(port);
+    }
+    stop() {
+    this.server.close();
+    }
+}
